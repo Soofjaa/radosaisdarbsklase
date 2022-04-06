c800cb7575e0971326d7ef0ccdf673c8df0a1678
Soofjaa <102229179+Soofjaa@users.noreply.github.com>
2022-04-06 16:12:00 +0300

    labojumsardef

---
+++ rinkis.py
@@ -0,0 +1,17 @@
+import math 
+ 
+def rinkislaukums (rad):
+    laukrink = math.pi*rad*rad 
+    return laukrink
+
+def rinkislinijagarums (rad):
+    rinklin = (math.pi*2*rad)
+    return rinklin
+
+def radiussnolinijas (radlin):
+    radgar = (radlin//(2*math.pi))
+    return radgar
+
+def radiussnolaukumaa (radlauk):
+    radgarr = (math.sqrt(radlauk//math.pi))
+    return radgarr
--- sakumsarrinki.py
+++ sakumsarrinki.py
@@ -1,25 +1,37 @@
 import math
-figura = int(input("Ar kādu figūru gribi darboties? Riņķis(1), četrstūris(2), trīsstūris (3)"))
+import rinkis
+import trijsturis
+import cetrsturis
+
+figura = int(input("Ar kādu figūru gribi darboties? Riņķis(1), četrstūris(2), trīsstūris (3)?"))
 if figura == 1:
-  figurarek = int(input("Ko gribi rekināt, laukumu(11), radiusu(12) vai rinķa līniju(13)"))
+  figurarek = int(input("Ko gribi rekināt, laukumu(11), radiusu(12) vai rinķa līnijas garumu(13)"))
 
   if figurarek == 11:
     rad = float(input("ievadi riņķa rādiusu!"))
-    laukrink = (math.pi*rad*rad)
-    print("laukums riņķim ir", laukrink , "!")
+    print("laukums riņķim ir", rinkis.rinkislaukums(rad) , "!")
 
   if figurarek == 13:
     rad = float(input("ievadi riņķa rādiusu!"))
-    rinklin = (math.pi*2*rad)
-    print("riņķa līnijas garums ir", rinklin , "!")
+    print("riņķa līnijas garums ir", rinkis.rinkislinijagarums(rad) , "!")
 
   if figurarek == 12:
     radapr = float(input("Caur kādu lielumu rēķināsim, riņķa līnijas garumu(121) vai riņķa laukumu(122)!"))
+
     if radapr == 121:
       radlin = float(input("ievadi riņķa līnijas garumu!"))
-      radgar = (radlin//(2*math.pi))
-      print("riņķa radiuss ir", radgar , "!")
+      print("riņķa radiuss ir", rinkis.radiussnolinijas(radlin) , "!")
+
     if radapr == 122:
-      radlauk = float(input("ievadi riņķa laukumu!"))
-      radgarr = (math.sqrt(radlauk//math.pi))
-      print("riņķa radiuss ir", radgarr , "!")
+      radlauk = float(input("ievadi riņķa laukumu!")) 
+      print("riņķa radiuss ir", rinkis.radiussnolaukumaa(radlauk) , "!")
+
+
+if figura == 2:
+  figurarek = int(input("Ko gribi rekināt, laukumu(21), diagonāli(22) vai perimetru(23)?"))
+   if 
+
+
+if figura == 3:
+    figurarek = int(input("Ko gribi rekināt, laukumu(31), augstumu(32) vai perimetru(33)?"))
+  
