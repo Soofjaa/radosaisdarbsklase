ea1a6cf317ccd1d9587df589ae6e41ff690103aa
Soofjaa <102229179+Soofjaa@users.noreply.github.com>
2022-04-06 16:57:51 +0300

    pedejis labojums

--- sakumsarrinki.py
+++ sakumsarrinki.py
@@ -1,3 +1,4 @@
+
 import math
 import rinkis
 import trijsturis
@@ -8,30 +9,61 @@ if figura == 1:
   figurarek = int(input("Ko gribi rekināt, laukumu(11), radiusu(12) vai rinķa līnijas garumu(13)"))
 
   if figurarek == 11:
-    rad = float(input("ievadi riņķa rādiusu!"))
-    print("laukums riņķim ir", rinkis.rinkislaukums(rad) , "!")
+     rad = float(input("ievadi riņķa rādiusu!"))
+     print("laukums riņķim ir", rinkis.rinkislaukums(rad) , "!")
 
   if figurarek == 13:
-    rad = float(input("ievadi riņķa rādiusu!"))
-    print("riņķa līnijas garums ir", rinkis.rinkislinijagarums(rad) , "!")
+     rad = float(input("ievadi riņķa rādiusu!"))
+     print("riņķa līnijas garums ir", rinkis.rinkislinijagarums(rad) , "!")
 
   if figurarek == 12:
-    radapr = float(input("Caur kādu lielumu rēķināsim, riņķa līnijas garumu(121) vai riņķa laukumu(122)!"))
+     radapr = float(input("Caur kādu lielumu rēķināsim, riņķa līnijas garumu(121) vai riņķa laukumu(122)!"))
 
-    if radapr == 121:
+  if radapr == 121:
       radlin = float(input("ievadi riņķa līnijas garumu!"))
       print("riņķa radiuss ir", rinkis.radiussnolinijas(radlin) , "!")
 
-    if radapr == 122:
+  if radapr == 122:
       radlauk = float(input("ievadi riņķa laukumu!")) 
       print("riņķa radiuss ir", rinkis.radiussnolaukumaa(radlauk) , "!")
 
 
 if figura == 2:
-  figurarek = int(input("Ko gribi rekināt, laukumu(21), diagonāli(22) vai perimetru(23)?"))
-   if 
+  figurarek2 = int(input("Ko gribi rekināt, laukumu(21), diagonāli(22) vai perimetru(23)?"))
+
+  if figurarek2 == 21: 
+       a,b = float(input("ievadi četrstūra malu garumus!"))
+       print("četrstūra laukums  ir", cetrsturis.laukums(a,b) , "!")
+
+  if figurarek2 == 22:
+        a,b = float(input("ievadi četrstūra malu garumus!"))
+        print("četrstūra diagonāļu garums ir", cetrsturis.diagonalesgarums(a,b) , "!")
+
+  if figurarek2 == 23:
+        a,b = float(input("ievadi četrstūra malu garumus!"))
+        print("četrstūra parimetrs  ir", cetrsturis.perimetrs(a,b) , "!")
 
 
 if figura == 3:
-    figurarek = int(input("Ko gribi rekināt, laukumu(31), augstumu(32) vai perimetru(33)?"))
+  figurarek3 = int(input("Ko gribi rekināt, laukumu(31), augstumu(32) vai perimetru(33)?"))
+
+  if figurarek3 == 31:
+        laukveid = int(input("Kā gribi rekināt, caur Hērona teorēmu(311) vai caur augstumu?(312)"))
+
+        if laukveid == 311: 
+         a,b = float(input("ievadi trīsstūra malas un augstuma garumus!"))   
+         print("trīsstūra laukums ir", trijsturis.laukums(a,b) , "!")
+
+        if laukveid == 312:
+            a,b,c = float(input("ievadi trīsstūra malas garumus!"))
+            print("trīsstūra perimetrs ir", trijsturis.laukumstrissturis1(a,b,c) , "!")
+
+  if figurarek3 == 32:
+        a,b,c = float(input("ievadi trīsstūra malas garumus!"))
+        print("trīsstūra perimetrs ir", trijsturis.perimetrstrissturis(a,b,c) , "!")
+
+  if figurarek3 == 33:
+         a,p = float(input("ievadi trīsstūra malu un perimetru!"))   
+         print("trīsstūra austums ir", trijsturis.augstumstrissturis(a,p) , "!")
+
   
