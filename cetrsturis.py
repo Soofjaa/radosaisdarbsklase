523705684f52ed9e418383708f5a54e29bb8a783
babajsus <102524051+babajsus@users.noreply.github.com>
2022-04-06 15:08:14 +0300

    nice

--- cetrsturis.py
+++ cetrsturis.py
@@ -1,1 +1,10 @@
-def  
+import math
+math.sqrt()
+def perimetrs (a,b):
+    return (a)*2+(b)*2
+def laukums (a,b):
+    return (a)*(b) 
+def diagonalesgarums (a,b):
+    return math.sqrt((a*a)+(b*b))
+def apvilktaisrinkalinijasradiuss (a,b):
+    return math.sqrt((a*a)+(b*b))/2
